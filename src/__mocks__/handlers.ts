701a75cf83b8cfab04f6410f3a03677f1c27d27a
Dana <do.silver2@gmail.com>
2025-02-02 01:25:40 +0900

    🐛 Module externalized for browser compatibility 해결, get response 타입 수정

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,5 +1,3 @@
-import { randomUUID } from 'crypto';
-
 import { http, HttpResponse } from 'msw';
 
 import { Event, EventForm } from '../types';
@@ -8,14 +6,14 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get<never, never, Event[]>('/api/events', () => {
-    return HttpResponse.json(events as Event[]);
+  http.get<never, never, { events: Event[] }>('/api/events', () => {
+    return HttpResponse.json({ events: events as Event[] });
   }),
 
   http.post<never, EventForm, Event>('/api/events', async ({ request }) => {
     const event = (await request.json()) as EventForm;
-    const newEvent = { ...event, id: randomUUID() };
-    return HttpResponse.json(newEvent);
+    const newEvent = { ...event, id: Date.now().toString() };
+    return HttpResponse.json(newEvent, { status: 201 });
   }),
 
   http.put<{ id: string }, Event, Event>('/api/events/:id', async ({ request }) => {
