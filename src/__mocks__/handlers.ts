57cfcb31e0a1b16ac5d5dc820de83b8094e17b89
Dana <do.silver2@gmail.com>
2025-02-03 22:19:08 +0900

    🐛 Handlers에서 초기 데이터 설정되지 않는 문제 수정

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,12 +1,17 @@
 import { http, HttpResponse } from 'msw';
 
 import { Event, EventForm } from '../types';
-import { getEvents, setupMockHandlerDeletion, setupMockHandlerUpdating } from './handlersUtils';
-// FIXME: get에서 events를 사용하면서도 업데이트된 상태를 반환하도록 어떻게 수정할 수 있을까?
+import {
+  getEvents,
+  setupMockHandlerCreation,
+  setupMockHandlerDeletion,
+  setupMockHandlerUpdating,
+} from './handlersUtils';
 import { events } from './response/events.json' assert { type: 'json' };
 
-// ! HARD
-// ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
+// 초기값 설정
+setupMockHandlerCreation(events as Event[]);
+
 export const handlers = [
   http.get<never, never, { events: Event[] }>('/api/events', () => {
     return HttpResponse.json({ events: getEvents() });
